14ea488c33e4dd3c54bff98f60dd2fbd90eb2d44
gastonperego <gastonperego@gmail.com>
2024-01-03 16:22:53 +0000

    fix: border case

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     "OOOOOOOAAAAAA"
     li = []
     for i in range(n):
