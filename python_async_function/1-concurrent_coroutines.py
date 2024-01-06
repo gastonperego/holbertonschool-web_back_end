452c9cb20f3d4c754c9133a5680675cab0b3b230
gastonperego <gastonperego@gmail.com>
2024-01-06 16:38:53 +0000

    fix: the whole task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,13 +2,20 @@
 """EEEEEEEEOOOOOOOOO"""
 
 from typing import List
+import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     "OOOOOOOAAAAAA"
     li = []
     for i in range(n):
-        num = await wait_random(max_delay)
-        li.append(num)
-    return sorted(li)
+        task = asyncio.create_task(wait_random(max_delay))
+        li.append(task)
+
+    times = []
+    for task in asyncio.as_completed(li):
+        time = await task
+        times.append(time)
+
+    return times
