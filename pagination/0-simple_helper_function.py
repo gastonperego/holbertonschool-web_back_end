f33d91c183647505eeb7832746c6e236bdd89a4f
gastonperego <gastonperego@gmail.com>
2024-01-12 22:43:30 +0000

    fix: pyco

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """sssss"""
 
+
 def index_range(page: int, page_size: int) -> tuple:
     """dddddd"""
-    return (((page - 1) * page_size) , page * page_size)
+    return (((page - 1) * page_size), page * page_size)
