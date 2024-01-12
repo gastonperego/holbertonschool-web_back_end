9983a044b035bc5338b350649051c6ac37426b36
gastonperego <gastonperego@gmail.com>
2024-01-12 18:46:10 +0000

    feat: task 11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -6,8 +6,4 @@ import pymongo
 
 def schools_by_topic(mongo_collection, topic):
     """flllllllll"""
-    lis = []
-    for school in mongo_collection.find():
-        if topic in school["topics"]:
-            lis.append(school["name"])
-    return lis
+    return list(mongo_collection.find({"topics": topic}))
