a51acf5458bd2a6afc77678e643cd7bfcc81e12a
gastonperego <gastonperego@gmail.com>
2024-01-12 19:10:08 +0000

    feat: task 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,14 +4,15 @@
 import pymongo
 
 
-client = pymongo.MongoClient()
-nginx = client.logs.nginx
-num_of_docs = nginx.count_documents()
-print(f"{num_of_docs} logs")
-print("Methods:")
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for method in methods:
-    num_of_method = nginx.count_documents({"method": method})
-    print(f"\tmethod {method}: {num_of_method}")
-status = nginx.count_documents({"method": "GET"}, {"path": "/status"})
-print(f"{status} status chech")
+if __name__ == "__main__":
+    client = pymongo.MongoClient()
+    nginx = client.logs.nginx
+    num_of_docs = nginx.count_documents()
+    print(f"{num_of_docs} logs")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        num_of_method = nginx.count_documents({"method": method})
+        print(f"\tmethod {method}: {num_of_method}")
+    status = nginx.count_documents({"method": "GET"}, {"path": "/status"})
+    print(f"{status} status chech")
