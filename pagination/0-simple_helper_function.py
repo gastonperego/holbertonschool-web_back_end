1c68c8d5712d77de6bb25d1973a642d1bde0f4a6
gastonperego <gastonperego@gmail.com>
2024-01-12 22:42:12 +0000

    fix: annotations

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """sssss"""
 
-def index_range(page: int, page_size: int) -> tuple(int, int):
+def index_range(page: int, page_size: int) -> tuple:
     """dddddd"""
     return (((page - 1) * page_size) , page * page_size)
