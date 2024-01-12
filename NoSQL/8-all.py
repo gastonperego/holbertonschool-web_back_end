301bcb33e669bbbc159d0d2abfe4c2ac10372cb4
gastonperego <gastonperego@gmail.com>
2024-01-12 00:39:44 +0000

    feat: task 8

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -7,7 +7,7 @@ import pymongo
 def list_all(mongo_collection):
     """ddddddd"""
     client = pymongo.MongoClient()
-    print(client.mongo_collection.find)
+    return (client.mongo_collection.find())
     
 if __name__ == '__main__':
     list_all(mongo_collection)
