5c2ad27455895f640ce527e8011b6f94770a762b
gastonperego <gastonperego@gmail.com>
2024-01-21 19:57:52 +0000

    feat: task 12

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,13 @@
+export default function createReportObject(employeesList) {
+    return {
+        allEmployees: employeesList,
+        getNumberOfDepartments(employeesList) {
+            let num = 0
+            for (let department in employeesList) {
+                num += 1
+            }
+            return num
+        }
+            
+        }
+    }
