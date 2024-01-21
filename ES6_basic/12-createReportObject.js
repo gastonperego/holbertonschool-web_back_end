3fc99e11856311730fe9dacf5e5354eb340e21bd
gastonperego <gastonperego@gmail.com>
2024-01-21 22:42:19 +0000

    fix: everyyhing I could of lint

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -2,11 +2,7 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: employeesList,
     getNumberOfDepartments(employeesList) {
-      let num = 0
-      for (let department in employeesList) {
-        num += 1
-      }
-      return num
+      return Object.values(employeesList).length;
     }
   }
 }
