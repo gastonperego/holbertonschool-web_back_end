77ce89c0710ea3421706f9cf142053455e453a1e
gastonperego <gastonperego@gmail.com>
2024-01-26 16:45:32 +0000

    fix: catch added

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,5 +3,6 @@ import { createUser, uploadPhoto } from './utils.js';
 export default function handleProfileSignup() {
   Promise
     .all([uploadPhoto(), createUser()])
-    .then((results) => { console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`); });
+    .then((results) => { console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`); })
+    .catch(() => console.log('Signup system offline'))
 }
