aadafbfae05ff9a6499bce62edac7ae1f4ea6855
gastonperego <gastonperego@gmail.com>
2024-01-26 13:29:08 +0000

    fix: eslint

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,11 +1,10 @@
 export default function getFullResponseFromAPI(success) {
-    return (new Promise((resolve, reject)  => {
-        const value = success
-        if (value == true) {
-            resolve ({status: 200, body: "Success"})
-        }
-        else {
-            reject (new Error("The fake API is not working currently"))
-        }
-    }))
+  return (new Promise((resolve, reject) => {
+    const value = success;
+    if (value === true) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  }));
 }
