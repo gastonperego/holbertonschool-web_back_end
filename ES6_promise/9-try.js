4a11b32f429184d47112ad022f4d5922bda719e4
gastonperego <gastonperego@gmail.com>
2024-01-27 04:19:12 +0000

    fix: string

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -4,7 +4,7 @@ export default function guardrail(mathFunction) {
     queue.push(mathFunction(), 'Guardrail was processed');
     return queue;
   } catch (error) {
-    queue.push(error.message, 'Guardrail was processed');
+    queue.push(`Error: ${error.message}`, 'Guardrail was processed');
     return queue;
   }
 }
