b6ec1251526988f63cb02c186aa8e968aadd840c
gastonperego <gastonperego@gmail.com>
2024-01-28 19:30:26 +0000

    fix: reutrn

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,12 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
-      console.log('Got a response from the API');
       return { status: 200, body: 'Success' };
     })
     .catch(() => {
-      console.log('Got a response from the API');
       return new Error();
-    });
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
+    })
 }
