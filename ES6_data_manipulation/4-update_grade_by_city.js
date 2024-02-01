c31f9493ca6f5608f7e2e61ade4d76b0fcacb5d9
gastonperego <gastonperego@gmail.com>
2024-02-01 23:49:34 +0000

    fix: lint

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,6 @@
 export default function updateStudentGradeByCity(students, city, grades) {
-  let filtered_students = students.filter((student) => student.location === city)
-  let updated = filtered_students.map((student) => {
+  const filteredStudents = students.filter((student) => student.location === city);
+  const updated = filteredStudents.map((student) => {
     let grade = 'N/A';
     for (const g of grades) {
       if (g.studentId === student.id) {
@@ -9,5 +9,5 @@ export default function updateStudentGradeByCity(students, city, grades) {
     }
     return { ...student, grade };
   });
-  return updated
+  return updated;
 }
