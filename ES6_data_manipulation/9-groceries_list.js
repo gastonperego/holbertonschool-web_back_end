7de41793ed156cc0cc5bfecece2fbb67c3bb0d79
gastonperego <gastonperego@gmail.com>
2024-02-02 01:29:41 +0000

    fix: quotation marks

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,12 @@
-export default function groceriesList() {
+//export default 
+function groceriesList() {
     const groceriesList = new Map()
-    groceriesList.set(Apples, 10);
-    groceriesList.set(Tomatoes, 10);
-    groceriesList.set(Pasta, 1);
-    groceriesList.set(Rice, 1);
-    groceriesList.set(Banana, 5);
+    groceriesList.set('Apples', 10);
+    groceriesList.set('Tomatoes', 10);
+    groceriesList.set('Pasta', 1);
+    groceriesList.set('Rice', 1);
+    groceriesList.set('Banana', 5);
     return groceriesList
 }
+
+console.log(groceriesList());
