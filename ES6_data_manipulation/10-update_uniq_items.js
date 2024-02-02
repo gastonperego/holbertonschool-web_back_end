8eb59f862ca89ea3e9de2ba94ebdf3246ebe6bea
gastonperego <gastonperego@gmail.com>
2024-02-02 01:44:01 +0000

    fix: eslint and not a map

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,8 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
   for (const array of map) {
     if (array[1] === 1) {
-        map.set(array[0], 100);
+      map.set(array[0], 100);
     }
   }
   return map;
