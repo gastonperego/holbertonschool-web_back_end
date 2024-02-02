21361f85e63ca27d17da05b8b98bd74036568a97
gastonperego <gastonperego@gmail.com>
2024-02-02 01:32:31 +0000

    fix: eslint

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,9 @@
 export default function groceriesList() {
-    const groceriesList = new Map()
-    groceriesList.set('Apples', 10);
-    groceriesList.set('Tomatoes', 10);
-    groceriesList.set('Pasta', 1);
-    groceriesList.set('Rice', 1);
-    groceriesList.set('Banana', 5);
-    return groceriesList
+  const groceriesList = new Map();
+  groceriesList.set('Apples', 10);
+  groceriesList.set('Tomatoes', 10);
+  groceriesList.set('Pasta', 1);
+  groceriesList.set('Rice', 1);
+  groceriesList.set('Banana', 5);
+  return groceriesList;
 }
