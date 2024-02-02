a243c20832558c278681582666572a13ec25d4a3
gastonperego <gastonperego@gmail.com>
2024-02-02 01:18:29 +0000

    fix: non string passed and eslint??

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,13 @@
-export default function cleanSet(set, startString){
-  if (startString === undefined || startString === "") {
-    return "";
+export default function cleanSet(set, startString) {
+  if (startString === undefined || startString === '' || typeof(startString) != 'String') {
+    return '';
   }
-  let string = "";
+  let string = '';
   for (const element of set) {
     if (element.startsWith(startString)) {
-      string += element.slice((element.length - (element.length - startString.length)), element.length) + "-";
-    };
-  };
+      string = string + element.slice((element.length - (element.length - startString.length)), element.length) + '-';
+    }
+  }
   string = string.slice(0, string.length - 1);
   return string;
 }
