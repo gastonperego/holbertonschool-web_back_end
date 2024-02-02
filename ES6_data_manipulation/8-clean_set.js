8927b5c46959529478cfc7e60bcee161f09d9a0e
gastonperego <gastonperego@gmail.com>
2024-02-02 01:12:02 +0000

    feat: task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+//export default 
+function cleanSet(set, startString){
+  if (startString === undefined || startString === "") {
+    return "";
+  }
+  let string = "";
+  for (const element of set) {
+    if (element.startsWith(startString)) {
+      string += element.slice((element.length - (element.length - startString.length)), element.length) + "-";
+    };
+  };
+  string = string.slice(0, string.length - 1);
+  return string;
+}
