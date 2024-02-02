82552631a2e214d9b916774fc92616f43c80a760
gastonperego <gastonperego@gmail.com>
2024-02-02 01:21:51 +0000

    fix: non string type

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,17 @@
-export default function cleanSet(set, startString) {
-  if (startString === undefined || startString === '' || typeof(startString) != 'String') {
+//export default 
+function cleanSet(set, startString) {
+  if (startString === undefined || startString === '' || typeof(startString) !== 'string') {
     return '';
   }
   let string = '';
   for (const element of set) {
     if (element.startsWith(startString)) {
-      string = string + element.slice((element.length - (element.length - startString.length)), element.length) + '-';
+      string += element.slice((element.length - (element.length - startString.length)), element.length) + '-';
     }
   }
   string = string.slice(0, string.length - 1);
   return string;
 }
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
